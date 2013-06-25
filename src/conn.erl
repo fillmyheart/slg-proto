a29ef77734d0234a9827f1e0c52d9ac8ca15ac6d
ianliao <lcfillmyheart@163.com>
2013-06-25 21:52:47 +0800

    heartbeat没有找到调用接口

--- src/conn.erl
+++ src/conn.erl
@@ -15,16 +15,23 @@
 -record(state, {
           recv_cnt = 0,                  %% 读取到的数据个数
           socket,                        %% 与进程关联的 socket
-          player_id                      %% 玩家ID
-         }).
+          player_id,                     %% 玩家ID
+          keep_still_times = 0           %% 超越次数
+  	}).
 
 -include("proto.hrl").
 
+%% 启动gen_server
 start_link(Socket) ->
   gen_server:start_link(?MODULE, [Socket], []).
 
+%% 停止gen_server
 stop(Ref) ->
   gen_server:cast(Ref, stop).
+
+%% 检查连接的数据包
+heart_beat(Pid) -> 
+  gen_server:call(Pid, heart_beat).
 
 init([Socket]) ->
   process_flag(trap_exit, true),
@@ -37,6 +44,20 @@ handle_cast(Cast, State) ->
   State1 = conn_config:exec(cast, [Cast, State]),
   {noreply, State1}.
 
+%% 检查连接是否超时仍为有数据通信
+handle_call(heart_beat, _From, State = #state{socket = Socket,
+  recv_cnt = ReceiveBags, keep_still_times = LostTimes}) ->
+  NowConnBags = inet:getstat(Socket, [recv_cnt]),
+  case ReceiveBags == NowConnBags of
+    true -> NewSate = #state{socket = Socket, keep_still_times = LostTimes + 1};
+    false -> NewState = #state{socket = Socket, recv_cnt = NowConnBags, keep_still_times = 0}
+  end,
+  case LostTimes >= ?LOSTTIME of
+    true -> gen_tcp:close(Socket),
+      {stop, timeout, NewState};
+    false -> {noreply, NewState}
+  end.
+
 handle_call(Call, From, State) ->
   State1 = conn_config:exec(cast, [Call, From, State]),
   {reply, ok, State1}.
@@ -47,6 +68,7 @@ handle_info({tcp_closed, Socket}, State)
 
 handle_info({tcp_error, _Port, _Reason}, State) ->
   {noreply, State};
+
 
 handle_info({tcp, Socket, Bin}, State) ->
   inet:setopts(Socket, [{active, once}, {packet, 2}, binary]),
@@ -79,3 +101,6 @@ send(TypeAtom, Paylod) ->
   Bin = proto_encoder:encode(TypeAtom, Paylod),
   gen_tcp:send(Socket, Bin),
   ok.
+
+  
+  
